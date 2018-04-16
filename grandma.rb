91caafab975e754dd00ff57e32db0087cbf7aa90
William J Teskey <willjteskey@gmail.com>
2018-04-16 20:04:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
